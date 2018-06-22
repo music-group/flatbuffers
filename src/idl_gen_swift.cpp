9815ac044f4a28becb13a4d2700d65bf33b1752e
Martin O'Shea <martin.oshea@music-group.com>
2018-06-22 09:29:42 +0100

    Add the ability to swift to generate tables with the setters and getters required for internal table values.
    This does not include work for source includes or strings or vectors

--- src/idl_gen_swift.cpp
+++ src/idl_gen_swift.cpp
@@ -165,6 +165,7 @@ class SwiftGenerator : public BaseGenerator {
   void BeginFile(const std::string name_space_name, std::string *code_ptr) {
     std::string &code = *code_ptr;
     code = code + "// " + FlatBuffersGeneratedWarning() + "\n\n";
+    code += "import Foundation\n";
     code += "import FlatBuffers\n\n";
   }
 
@@ -236,9 +237,22 @@ class SwiftGenerator : public BaseGenerator {
 
     std::string &code = *code_ptr;
 
-    code += "protocol ";
+    code += "struct ";
     code += struct_def.name;
+    code += ": Table";
     code += " {\n";
+
+    // Add book keeping variables from Table protocol
+    code += "    var data: Data\n";
+
+    code += "    var tablePosition: UOffset = 0\n";
+
+    code += "\n";
+
+    // add struct constructor for parsing
+    code += "    init(data: Data) {";
+    code += " self.data = Data ";
+    code += "}\n\n";
 
     // Generate struct fields accessors
     for (auto it : struct_def.fields.vec) {
@@ -251,18 +265,161 @@ class SwiftGenerator : public BaseGenerator {
         GenComment(field.doc_comment, code_ptr, nullptr, "    ");
       }
 
-      code += "    var ";
+      // TODO: Make var getters work for properties that are not ints
+      code += "    public var ";
       code += MakeCamel(field.name, false);
 
       code += ": ";
 
       code += GenType(field.value.type);
 
-      code += " { get }\n";
+
+      code += " {\n";
+      code += "        get {\n";
+      code += "            let tableValue = offset(vtableElementIndex: ";
+      code += NumToString(field.value.offset);
+      code += ") \n";
+      code += "            return tableValue != 0 ? data.getIntegerType(uoffset:tableValue + tablePosition) : ";
+      code += field.value.constant;
+      code += "\n";
+      code += "        }\n";
+      code += "    }\n";
     }
 
-    code += "}\n\n";
+    // Generate a table constructor of the form:
+    // public static int createName(FlatBufferBuilder builder, args...)
+    code += "\n    public static func ";
+    code += "create" + struct_def.name;
+    code += "(builder: Builder";
+    for (auto it = struct_def.fields.vec.begin();
+         it != struct_def.fields.vec.end(); ++it) {
+      auto &field = **it;
+      if (field.deprecated) continue;
+      code += ",\n                                ";
+      code += field.name;
+      code += ": ";
+      code += GenType(field.value.type);
+      if (!IsScalar(field.value.type.base_type)) code += "UOffset";
+    }
+
+    code += ") -> UOffset {\n        builder.";
+    code += "startObject(feildCount: ";
+    code += NumToString(struct_def.fields.vec.size()) + ")\n";
+    for (size_t size = struct_def.sortbysize ? sizeof(largest_scalar_t) : 1;
+         size; size /= 2) {
+      for (auto it = struct_def.fields.vec.rbegin();
+           it != struct_def.fields.vec.rend(); ++it) {
+        auto &field = **it;
+        if (!field.deprecated &&
+            (!struct_def.sortbysize ||
+                size == SizeOf(field.value.type.base_type))) {
+          code += "        " + struct_def.name + ".";
+          code += "add";
+          code += MakeCamel(field.name) + "(builder, " + field.name;
+          if (!IsScalar(field.value.type.base_type)) code += "Offset";
+          code += ")\n";
+        }
+      }
+    }
+    code += "        return " + struct_def.name + ".";
+    code += "end" + struct_def.name;
+    code += "(builder)\n    }\n\n";
+
+    generateClassStructStaticMethods(struct_def, code_ptr);
   }
+
+  void generateClassStructStaticMethods(const StructDef &struct_def, std::string *code_ptr) {
+
+    std::string &code = *code_ptr;
+    // Generate a set of static methods that allow table construction,
+    // of the form:
+    // public static func addName(_ builder: Builder, _ value: Int32)
+    // { builder.add(vTableIndex: 0, value: value, defaultValue: 100) }
+    // Unlike the Create function, these always work.
+    code += "    public static func start";
+    code += struct_def.name;
+    code += "(builder: Builder) { builder.";
+    code += "startObject(feildCount: ";
+    code += NumToString(struct_def.fields.vec.size()) + ") }\n";
+    for (auto it = struct_def.fields.vec.begin();
+         it != struct_def.fields.vec.end(); ++it) {
+      auto &field = **it;
+      if (field.deprecated) continue;
+      code += "    public static func add";
+      code += MakeCamel(field.name);
+      code += "(_ builder: Builder, ";
+      auto argname = MakeCamel(field.name, false);
+      if (!IsScalar(field.value.type.base_type)) argname += "Offset";
+      code += "_ " + argname + ": " + GenType(field.value.type) + ") { builder." + "add";
+      code += "(vTableIndex: ";
+      code += NumToString(it - struct_def.fields.vec.begin()) + ", " + argname + ": ";
+      code += argname;
+      code += ", defaultValue: ";
+      code += field.value.constant;
+      code += ") }\n";
+
+      // TODO: add code for generating vectos in table
+    }
+    code += "    public static func ";
+    code += "end" + struct_def.name;
+    code += "(_ builder: Builder) -> UOffset {\n        let o = builder.";
+    code += "endObject()\n";
+    for (auto it = struct_def.fields.vec.begin();
+         it != struct_def.fields.vec.end(); ++it) {
+      auto &field = **it;
+      if (!field.deprecated && field.required) {
+        code += "    builder.required(o, ";
+        code += NumToString(field.value.offset);
+        code += ")  // " + field.name + "\n";
+      }
+    }
+    code += "        return o\n    }\n";
+    if (parser_.root_struct_def_ == &struct_def) {
+        code += "    public static func ";
+        code += "finish" + struct_def.name;
+        code += "Buffer(builder: Builder, offset: UOffset) {";
+        code += " builder.finish(rootTable: offset";
+
+        if (parser_.file_identifier_.length()) {
+          code += ", \"" + parser_.file_identifier_ + "\"";
+        }
+        code += ") }\n";
+    }
+  // Only generate key compare function for table,
+  // because `key_field` is not set for struct
+  if (struct_def.has_key && !struct_def.fixed) {
+    code += "\n  protected int keysCompare(";
+    code += "Integer o1, Integer o2, ByteBuffer _bb) {";
+
+    code += " }\n";
+
+    code += "\n  public static " + struct_def.name;
+    code += " __lookup_by_key(";
+    code += "int vectorLocation, ";
+
+    code += " key, ByteBuffer bb) {\n";
+
+    code += "    int span = ";
+    code += "bb.GetInt(vectorLocation - 4)\n";
+    code += "    var start: UOffset = 0\n";
+    code += "    while (span != 0) {\n";
+    code += "      int middle = span / 2;\n";
+    code += "      if (comp > 0) {\n";
+    code += "        span = middle;\n";
+    code += "      } else if (comp < 0) {\n";
+    code += "        middle++;\n";
+    code += "        start += middle;\n";
+    code += "        span -= middle;\n";
+    code += "      } else {\n";
+    code += "        return ";
+    code += ".__assign(tableOffset, bb);\n";
+    code += "      }\n    }\n";
+    code += "    return null;\n";
+    code += "  }\n";
+  }
+  code += "}";
+  code += "\n\n";
+}
 
   // Return a Swift type from the table in idl.h
   std::string GenTypeBasic(const Type &type, bool user_facing_type) const {
